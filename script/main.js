e3691187ada0c903b878c9f3bf967a88f1383289
Pietro Torrico <torricopietro@gmail.com>
2023-07-08 06:50:31 -0500

    bebe happy birthday

--- script/main.js
+++ script/main.js
@@ -1,21 +1,17 @@
 // trigger to play music in the background with sweetalert
 window.addEventListener('load', () => {
     Swal.fire({
-        title: 'Do you want to play music in the background?',
+        title: '¿Estas lista para lo que viene? Sube el volumen de tu celu',
         // text: "You won't be able to revert this!",
-        icon: 'warning',
+        icon: 'question',
         showCancelButton: true,
+        cancelButtonColor: '#00a000',
         confirmButtonColor: '#3085d6',
-        cancelButtonColor: '#d33',
-        confirmButtonText: 'Yes',
-        cancelButtonText: 'No',
+        confirmButtonText: 'Creo que sí',
+        cancelButtonText: '¡Claro que sí!'
     }).then((result) => {
-        if (result.isConfirmed) {
-            document.querySelector('.song').play();
-            animationTimeline();
-        } else {
-            animationTimeline();
-        }
+        document.querySelector('.song').play();
+        animationTimeline();
     });
 });
 
